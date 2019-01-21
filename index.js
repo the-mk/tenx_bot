76feb7e23e0457e42b6ecc2d9ef4bdd862339af8
the-mk <international.bluray@googlemail.com>
2019-01-21 23:14:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-var static = require('node-static');
-var file = new static.Server();
-require('http').createServer(function(request, response) {
-  request.addListener('end', function() {
-    file.serve(request, response);
-  }).resume();
-}).listen(process.env.PORT || 3000);
-
 var Twit = require('twit');
 var fs = require('fs');
 require('dotenv').config();
