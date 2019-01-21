9fd8ed6bb7b99772a7b8ffc2b2e76bc873d3804c
the-mk <international.bluray@googlemail.com>
2019-01-21 22:52:37 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# TenX "When Card" Service Bot
+
+This Bot automatically replies to frequent questions in tweets about the TenX card delivery.
+
+## Built With
+
+* [NodeJS](https://github.com/nodejs/node) - JavaScript runtime
+* [Twit](https://github.com/ttezel/twit) - Twitter API Client for node
+
+## Versioning
+
+0.1 - Bot replies to tweets which contain "When Cards" and "@tenxwallet"
---
+++ index2.js
@@ -0,0 +1,197 @@
+var Twit = require('twit');
+var fs = require('fs');
+require('dotenv').config();
+var T = new Twit({
+    consumer_key: process.env.BOT_CONSUMER_KEY,
+    consumer_secret: process.env.BOT_CONSUMER_SECRET,
+    access_token: process.env.BOT_ACCESS_TOKEN,
+    access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+});
+
+console.log("Initial Run started");
+fullRun()
+console.log("Initial Run completed");
+console.log("------------------------------------");
+console.log("Interval Run started");
+var run=0;
+setInterval(function(){
+ 	run = run + 1;
+  	console.log("Run " + run + " started");
+	fullRun();
+}, 300000);
+
+function fullRun() {
+ 	getTweets('@tenxwallet when ?');
+ 	getTweets('@julianhosp when ?');
+	retweetTenX();
+}
+
+async function getTweets(query) {
+	// Set parameters for the Twitter status query
+	var params = {
+	  q: query,  // REQUIRED
+	  result_type: 'recent'
+	}
+
+	var response = false;
+
+	return T.get('search/tweets', params, function (err, data) {})
+	.then(
+		function(data) {
+			 tweets = data.data.statuses;
+			 tweetsToReply = [];
+			 for (tweet in tweets) {
+			 	if (isTenX(tweets[tweet].user.id_str)) {continue;}
+			 	currentTweet = tweets[tweet];
+			 	if (region = hasCountry(currentTweet.text)) {
+			 		response = '@' + currentTweet.user.screen_name + ' ' + region[1] + ' is ' + isSupported(region) + ' region. First cards will ship to Singapore, followed by APAC, Europe and the rest of the World. Stay tuned for further announcements. Get involved at https://chat.tenx.tech';
+			 		tweetsToReply[currentTweet.id_str] = response;
+			 	} else if (hasKeyword(currentTweet.text)) {
+			 		response = '@' + currentTweet.user.screen_name + ' ' + 'Currently there is no set timeframe for "When cards". Stay tuned for further announcements. Get involved at https://chat.tenx.tech';
+			 		tweetsToReply[currentTweet.id_str] = response;
+			 	}
+			 }
+			 replyToTweets(tweetsToReply);
+		}
+	);
+}
+
+function replyToTweets(tweets) {
+	newReplied = [];
+	for (tweet in tweets) {
+		// console.log('Status: ' + tweets[tweet]);
+		// console.log('---------------------');
+		if (notRepliedYet(tweet) && (newReplied.indexOf(tweet) == '-1')) {
+			console.log('New Reply to Tweet ' + tweet);
+			newReplied.push(tweet);
+	        T.post('statuses/update', {
+	          status: tweets[tweet],
+	          in_reply_to_status_id: tweet,
+	        }, function (err, data, response) {
+	          if (err) {
+	          	console.log(err);
+	          } else {
+	          }
+	        });
+		}
+	}
+    if (newReplied.length) {
+      // console.log(newReplied);
+      for(var j = 0; j < newReplied.length; j++) {
+        var note = ',' + newReplied[j];
+        fs.appendFile('repliedTo.txt', note, (err) => {  
+            if (err) throw err;
+            else {
+            	// console.log('Tweet ' + newReplied[j] + ' successfully written to file.');
+            }
+        });  
+      }
+    }
+}
+
+function notRepliedYet(tweetID) {
+	var fs = require('fs');
+    var alreadyReplied = fs.readFileSync('repliedTo.txt').toString().split(",");
+    return alreadyReplied.indexOf(tweetID) == '-1';
+}
+
+function isTenX(userID) {
+	return userID == '4585412124';
+}
+
+function hasKeyword(tweet) {
+	tweet = tweet.split(" ");
+	keywords = ['cards', 'card'];
+
+	for (word in tweet) {
+		tweet[word] = deleteLineEnding(tweet[word]);
+		for (keyword in keywords) {
+			if (tweet[word].toLowerCase() == keywords[keyword]) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+// Returns 0 (unsupported Country) or 1 (supported Country) and the Countryname as an array, otherwise returns false
+function hasCountry(tweet) {
+	tweet = tweet.split(" ");
+	var supportedCountries = ['USA', 'Europe', 'EU', 'Asia', 'Aland Islands','Albania','Algeria','American Samoa','Andorra','Angola','Anguilla','Antarctica','Antigua and Barbuda','Argentina','Armenia','Aruba','Australia','Austria','Azerbaijan','Bahamas','Bahrain','Bangladesh','Barbados','Belarus','Belgium','Belize','Benin','Bermuda','Bhutan','Bolivia','Bosnia and Herzegovina','Botswana','Brazil','British Indian Ocean Territory','Brunei Darussalam','Bulgaria','Burkina Faso','Burundi','Cambodia','Cameroon','Canada','Cape Verde','Cayman Islands','Central African Republic','Chad','Chile','Christmas Island','Cocos (Keeling) Islands','Colombia','Comoros','Congo',    'Congo, The Democratic Republic of','Cook Islands','Costa Rica',"Cote d'Ivoire",'Croatia','Cyprus','Czech Republic','Denmark','Djibouti','Dominica','Dominican Republic','Ecuador','Egypt','El Salvador','Equatorial Guinea','Estonia','Ethiopia','Falkland Islands (Malvinas)','Faroe Islands','Fiji','Finland','France','French Guiana','French Polynesia','Gabon','Gambia','Georgia','Germany','Ghana','Gibraltar','Greece','Greenland','Grenada','Guadeloupe','Guam','Guatemala','Guernsey','Guinea-Bissau','Guinea','Guyana','Haiti','The Vatican','Honduras','Hong Kong','Hungary','Iceland','Ireland','Isle of Man','Israel','Italy','Jamaica','Japan','Jersey','Jordan','Kazakhstan','Kenya','Kiribati','South Korea','Kuwait','Kyrgyzstan','Laos','Latvia','Lesotho','Liberia','Liechtenstein','Lithuania','Luxembourg','Macao','Macedonia','Madagascar','Malawi','Malaysia','Maldives','Mali','Malta','Marshall Islands','Martinique','Mauritania','Mauritius','Mayotte','Mexico','Micronesia, Federated States of Micronesia','Moldova','Monaco','Mongolia','Montenegro','Montserrat','Morocco','Mozambique','Namibia','Nauru','Nepal','Netherlands','Netherlands Antilles','New Caledonia','New Zealand','Nicaragua','Niger','Nigeria','Niue','Norfolk Island','Northern Mariana Islands','Norway','Oman','Pakistan','Palau','Panama','Papua New Guinea','Paraguay','Peru','Philippines','Pitcairn','Poland','Portugal','Puerto Rico','Qatar','Romania','Russia','Rwanda','Reunion','Saint Barthelemy','Saint Helena, Ascension and Tristan Da Cunha','Saint Kitts and Nevis','Saint Lucia','Saint Martin','Saint Pierre and Miquelon','Saint Vincent and the Grenadines','Samoa','San Marino','Sao Tome and Principe','Saudi Arabia','Senegal','Serbia','Seychelles','Sierra Leone','Singapore','Slovakia','Slovenia','Solomon Islands','South Africa','South Georgia and the South Sandwich Islands','Spain','Sri Lanka','Suriname','Svalbard and Jan Mayen','Swaziland','Sweden','Switzerland','Taiwan','Tajikistan','Tanzania, United Republic of Tanzania','Thailand','Timor Leste','Togo','Tokelau','Tonga','Trinidad and Tobago','Tunisia','Turkey','Turkmenistan','Turks and Caicos Islands','Tuvalu','Uganda','Ukraine','United Arab Emirates','United Kingdom','United States','Uruguay','Uzbekistan','Vanuatu','Venezuela','Vietnam','Virgin Islands, British','Virgin Islands, U.S.','Wallis and Futuna','Zambia','Zimbabwe'];
+	var unsupportedCountries = ['China','India','Indonesia','Afghanistan','Cuba','Eritrea','Iran','Iraq','Kosovo', 'Lebanon','Libya','Myanmar','Northern Cyprus','North Korea','Palestine','Somalia','South Sudan','Sudan','Syria','Yemen'];
+	var countries = [unsupportedCountries, supportedCountries];
+
+	for (word in tweet) {
+		tweet[word] = deleteLineEnding(tweet[word]);
+		for (var c =  0; c < 2; c++) {
+		  var searchCountries = countries[c];
+		  //Check if Country is found in the Tweet 
+		  for (l = 0; l < searchCountries.length; l++) {
+		    //logger.info(substringArray[l]);
+		    if (tweet[word].toLowerCase() == searchCountries[l].toLowerCase()) {
+		      return [c, searchCountries[l]];
+		    }
+		  }
+		}
+	}
+	return false;
+}
+
+
+function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
+}
+
+function isSupported(region) {
+	if (region[0] == false) {
+		return 'an unsupported';
+	} else if (region[0] == true) {
+		return 'a supported';
+	} else {
+		return false;
+	}
+}
+
+async function checkTweetsFromTenx() {
+  var retweets = [];
+  await T.get('statuses/user_timeline', { user_id: '4585412124', count: 100, include_rts: false, exclude_replies: true }, function(err, data, response) {
+    for (var t =  0; t < data.length; t++) {
+
+      var searchArray = ['card', 'cards']
+      var retweet = false;
+
+      //Check if Substrings are found in string0
+      for (l = 0; l < searchArray.length; l++) {
+        //logger.info(substringArray[l]);
+        if (data[t].text.indexOf(searchArray[l]) > -1 && data[t].retweeted == false) {
+          retweet = true;
+          retweets.push(data[t].id_str);
+          break;
+        }
+      }
+    }
+  });
+  return retweets;
+}
+
+async function retweetTenX() {
+  retweetIDs = await checkTweetsFromTenx();
+  // console.log(retweetIDs);
+  for (var m = 0; m < retweetIDs.length; m++) {
+    T.post('statuses/retweet/:id', { id: retweetIDs[m] }, function (err, data, response) {
+      // console.log(data);
+    }); 
+  }
+};
+
+function deleteLineEnding(str) {
+	var lineEndings = ['?', '!', '.'];
+	for (ending in lineEndings) {
+		if (str.indexOf(lineEndings[ending]) != -1) {
+			str = str.substring(0, str.indexOf(lineEndings[ending]));
+		}
+	}
+	return str;
+}
+
+
