cea616ab7e6b72c45d1cba51091ddbdbf02ceebb
the-mk <international.bluray@googlemail.com>
2019-01-21 23:13:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+var static = require('node-static');
+var file = new static.Server();
+require('http').createServer(function(request, response) {
+  request.addListener('end', function() {
+    file.serve(request, response);
+  }).resume();
+}).listen(process.env.PORT || 3000);
+
 var Twit = require('twit');
 var fs = require('fs');
 require('dotenv').config();
@@ -17,7 +25,7 @@ if (test) {
   console.log("Interval run initiated");
   setInterval(function(){
     execute();
-  }, 300000); 
+  }, 30000); 
 }
 
 function execute() {
